3f840b2ba2ea7b13f9ea54554a8cc0fa79b6d754
Dee Bryant <tolkadot@gmail.com>
2017-04-07 09:40:08 +0000

    team styling and footer

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 	<div class="one-third text-center featured-heading"><h2 class="h2-footer"><a href="#contact_form_pop" class="fancybox">Contact Us</a>
 <div style="display:none" class="fancybox-hidden">
 <div id="contact_form_pop">
-       [contact-form-7 id="13" title="MMC"]</div>
+       <?php echo do_shortcode( '[contact-form-7 id="13" title="MMC"]' ); ?></div>
 </div></h3></div>
 	
 	
@@ -26,7 +26,7 @@
 	<div class="one-third text-center featured-heading"><h2 class="h2-footer">Business Scope</h3></div>
 
 </section>
-<div class="text-center weight-300"><?php echo(mdcl_copyright()) ?></div>
+<div class="text-center weight-300 copyright"><?php echo(mdcl_copyright()) ?></div>
 	</footer>
 </div>
 <?php wp_footer(); ?>
--- template--standard.php
+++ template--standard.php
@@ -9,14 +9,14 @@ get_header();?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 <!-- Swiper -->
-	<div class="page-header text-center"><h1> Our Team</h1></div>
+	
     <div class="swiper-container">
         <div class="swiper-wrapper">
             <div class="swiper-slide">          
             <div class="row">
 			    <div class="col-xs-12 col-sm-8">
                     <div class="person-text-item">
-                        <h4>Sean McMahon</h4>
+                        <h4 class="team-h4">Sean McMahon</h4>
                         Sean is a skilled Engineer, (BEng, MEng), with over 16 years experience working in the electronics and telecommunications sectors. Following his most recent position with f6 Tech working on the cloud based telematic solution in the AWS, he decided to pursue his dream of developing a collaborative medical network.
                     </div>
                 </div>
@@ -30,7 +30,7 @@ get_header();?>
             <div class="row">
 			    <div class="col-xs-12 col-sm-8">
                     <div class="person-text-item">
-                        <h4>George Gundry</h4>
+                        <h4 class="team-h4">George Gundry</h4>
    George is an experienced technical manager with over 30 years’ experience working in the IT and telecommunications sectors since graduating with a degree in Computer Science. He is an Agile Certified Practitioner, CSM and Prince 2 qualified Program & Project Manager with a sound technical background in software development, helping on & off shore teams deliver business value.                 </div>
                 </div>
                 <div class="col-xs-12 col-sm-4">
@@ -43,7 +43,7 @@ get_header();?>
             <div class="row">
 			    <div class="col-xs-12 col-sm-8">
                     <div class="person-text-item">
-                        <h4>Vincent Ryan</h4>
+                        <h4 class="team-h4">Vincent Ryan</h4>
                         Lecturer in Journalism with more than 8 years experience in national media. 
 Most recently worked on the development of a digital advertising concept with Saatchi & Saatchi for the Olympics.
 </div>
@@ -58,7 +58,7 @@ Most recently worked on the development of a digital advertising concept with Sa
             <div class="row">
 			    <div class="col-xs-12 col-sm-8">
                     <div class="person-text-item">
-                        <h4>Dee Bryant</h4>
+                        <h4 class="team-h4">Dee Bryant</h4>
         Deirdre is a full stack web developer. Since graduating with a BEng she has worked for major corporations such as O2 & Telefonica. She established a web development company in 2014.            </div>
                 </div>
                 <div class="col-xs-12 col-sm-4">
@@ -71,7 +71,7 @@ Most recently worked on the development of a digital advertising concept with Sa
             <div class="row">
 			    <div class="col-xs-12 col-sm-8">
                     <div class="person-text-item">
-                        <h4>Nigel Quinlan</h4>
+                        <h4 class="team-h4">Nigel Quinlan</h4>
               Nigel is an ux/ui designer with a passion for helping people and improving their lives. He blends design thinking with a background in science, manufacturing and Occupational Health & Science consultancy.          </div>
                 </div>
                 <div class="col-xs-12 col-sm-4">
